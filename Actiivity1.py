35a6a02efd45806fee67a0cc400b277f57a8aad7
Sharali-25 <Sharaliarkansas@gmail.com>
2026-05-31 10:07:46 -0500

    Add files via upload

---
+++ Actiivity1.py
@@ -0,0 +1,9 @@
+print("My name is Sharali Sharma")
+print("My favourite book is Dogman")
+print("My favorite food is cholle bhature,maggi,raita,rasmalai and kallaakand")
+print("addition of 2+5 is ",2+5)
+print("Hello CJP!\n")
+num=int(input("please enter ur number :"))
+num2= int(input("Please enter ur second number :"))
+sum= num+num2 
+print("The sum of the numbers is :",sum)
