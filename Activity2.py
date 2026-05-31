098d27c631fb94ac9d5fec3547ff74b70e8143d4
Sharali-25 <Sharaliarkansas@gmail.com>
2026-05-31 10:08:38 -0500

    Add files via upload

---
+++ Activity2.py
@@ -0,0 +1,12 @@
+num1=int(input("Please enter ur 1st number"))
+num2=int(input('Please enter ur 2nd number'))
+add=num1+num2
+subtract=num1-num2
+multiply=num1*num2
+divide=num1/num2
+floor_divide=num1//num2
+print("The sum of the numbers is :", add)
+print("The difference of the numbers is :", subtract)
+print("The product of the numbers is:", multiply)
+print("The quotient of the numbers is: ", divide)
+print("The answer to the floor division is", floor_divide)
